72ca28d32f9175b6b655c7c57aa94e560a7f351c
nicky-LV <14sitnikovsn@bluecoatstudent.org.uk>
2021-04-28 18:37:35 +0100

    Added SMA endpoint

--- API/urls.py
+++ API/urls.py
@@ -4,5 +4,6 @@ from .views.technical_analysis import *
 urlpatterns = [
     path('get-tickers/', get_tickers, name='get-tickers'),
     path('get-historical-data/<str:equity>/', get_historical_data, name='historical-data'),
-    path('get-technical-indicators/', get_technical_indicators, name='get-technical-indicators')
+    path('get-technical-indicators/', get_technical_indicators, name='get-technical-indicators'),
+    path('SMA/<str:equity_symbol>/<str:requested_timeframe>/', simple_moving_average, name='sma')
 ]
