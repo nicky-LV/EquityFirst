11a296bf102fc850cf98833ffdc344bf46f55d84
nicky-LV <14sitnikovsn@bluecoatstudent.org.uk>
2021-05-03 12:13:15 +0100

    Changed message broker from rabbitmq to redis

--- CryptoDash/celery.py
+++ CryptoDash/celery.py
@@ -3,7 +3,7 @@ from celery import Celery
 
 os.environ["DJANGO_SETTINGS_MODULE"] = "CryptoDash.settings"
 # RabbitMQ message broker
-app = Celery('CryptoDash', broker="amqp://localhost")
+app = Celery('CryptoDash', broker="redis://localhost:6379")
 
 # imports Celery-related settings from settings.py, starting with CELERY. (e.g. CELERY_BROKER_URL = ...)
 app.config_from_object("CryptoDash.celery_settings", namespace="CELERY")
