260f666c11073dfa4bfdbe3505186de80eb36a1b
nicky-LV <14sitnikovsn@bluecoatstudent.org.uk>
2021-05-08 22:24:13 +0100

    [Frontend] Added reusable components for the "Cards" for the client's dashboard.

---
+++ frontend/components/dashboard/cards/card.tsx
@@ -0,0 +1,23 @@
+import CardHeader from "./cardHeader";
+import {ReactJSXElement} from "@emotion/react/types/jsx-namespace";
+
+export default function Card(props : CardProps){
+    return (
+        <div className="bg-white flex flex-column border-b border-gray-200 rounded-md h-full w-full">
+            <CardHeader header={props.header}>
+                {props.headerContent}
+            </CardHeader>
+
+            <div className="px-4 py-4">
+                {props.children}
+            </div>
+        </div>
+    )
+}
+
+interface CardProps{
+    header: string | ReactJSXElement
+    headerContent?: ReactJSXElement
+    children?: ReactJSXElement
+
+}
