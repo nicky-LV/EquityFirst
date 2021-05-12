e678830014a78965124696993ddebf9744e6c0f7
nicky-LV <14sitnikovsn@bluecoatstudent.org.uk>
2021-05-12 15:42:21 +0100

    Changed color of divider

--- frontend/components/divider.tsx
+++ frontend/components/divider.tsx
@@ -2,10 +2,10 @@ export default function Divider(props) {
   return (
     <div className="relative my-4">
       <div className="absolute inset-0 flex items-center" aria-hidden="true">
-        <div className="w-full border-t border-gray-300" />
+        <div className="w-full border-t border-gray-400" />
       </div>
       <div className="relative flex justify-center">
-        <span className="px-2 bg-white text-sm text-gray-500">{props.text}</span>
+        <span className="px-2 bg-white text-sm text-gray-800">{props.text}</span>
       </div>
     </div>
   )
