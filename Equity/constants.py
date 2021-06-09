10ea25e5f970fc320a1a18931e7cd83bc15c3301
nicky-LV <nickystnkvs@outlook.com>
2021-06-09 13:50:49 +0100

    Added description to technical indicators.

--- Equity/constants.py
+++ Equity/constants.py
@@ -1,4 +1,18 @@
 equity_symbols = ['AAPL', 'MSFT', 'AMZN', 'GOOGL', 'TSLA', 'FB', 'NVDA', 'PYPL', 'NFLX', 'CMCSA']
-technical_indicators = ["SMA", "EMA", "MACD", "STOCH", "RSI", "ADX"]
+# todo: add "type" of indicator being either "lagging" or "leading".
+technical_indicators = [
+    {"name": "SMA",
+     "description": "Simple moving average"},
+    {"name": "EMA",
+     "description": "Exponential moving average"},
+    {"name": "MACD",
+     "description": "Moving average convergence divergence"},
+    {"name": "STOCH",
+     "description": "Stochastic oscillator"},
+    {"name": "RSI",
+     "description": "Relative strength index"},
+    {"name": "ADX",
+     "description": "Average directional index"}
+]
 timescales = {"1D": 1, "1W": 7, "1M": 30, "3M": 90, "6M": 180, "1Y": 365, "YTD": "YTD"}
 ma_types = ["SMA", "MA"]
