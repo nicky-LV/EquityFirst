ce838f1db8d5224d27c75a08434c02367871f5ad
nicky-LV <nickystnkvs@outlook.com>
2021-06-30 22:14:12 +0100

    [Backend] Added tests

--- Equity/tests.py
+++ Equity/tests.py
@@ -3,7 +3,9 @@ import random
 
 from Redis.classes import Redis
 
-from .classes import Equity
+from .constants import equity_symbols
+
+from .classes import Equity, EquityTechnicalInfo
 from .utils import *
 from .exceptions import *
 
@@ -72,3 +74,54 @@ class TestEquity:
         # Retrieves historical data
         hist_data = eq_object.historical_data
         last_entry = hist_data[-1]
+
+
+def test_sma(equity_symbol_and_instance):
+    """ Tests getting / setting of SMAs """
+    equity_symbol, instance_ = equity_symbol_and_instance
+    obj = EquityTechnicalInfo(equity=equity_symbol)
+
+    # asserts that sma data is available and that its type is correct.
+    assert obj.sma is not None
+    assert type(obj.sma) == list
+    assert type(obj.sma[0]['SMA']) == int or type(obj.sma[0]['SMA']) == float
+
+    # caches sma data in the database
+    sma_data = get_technical_data(equity=equity_symbol, technical_indicator="SMA")
+    obj.sma = sma_data
+
+    # asserts that the data exists in the database.
+    cached_data = get_cached_data(key=f"{equity_symbol}-SMA")
+    dates_array = [data['date'] for data in cached_data]
+    random_date = sma_data[random.randint(0, len(sma_data)-1)]['date']
+
+    assert random_date in dates_array
+
+
+def test_technical_data(equity_symbol_and_instance):
+    """ Tests getting / setting of technical data for one randomly selected equity symbol. """
+    equity_symbol, instance_ = equity_symbol_and_instance
+    obj = EquityTechnicalInfo(equity=equity_symbol)
+
+    for technical_indicator in technical_indicators:
+        technical_indicator = technical_indicator['name']
+        if technical_indicator == "MACD":
+            pass
+
+        else:
+            # asserts that technical data is available and that its type is correct.
+            assert getattr(obj, technical_indicator.lower()) is not None
+            assert type(getattr(obj, technical_indicator.lower())) == list
+            assert type(getattr(obj, technical_indicator.lower())[0][technical_indicator]) == int or \
+                   type(getattr(obj, technical_indicator.lower())[0][technical_indicator]) == float
+
+            # caches technical data in the database
+            technical_data = get_technical_data(equity=equity_symbol, technical_indicator=technical_indicator)
+            setattr(obj, technical_indicator, technical_data)
+
+            # asserts that the data exists in the database.
+            cached_data = get_cached_data(key=f"{equity_symbol}-{technical_indicator}")
+            dates_array = [data['date'] for data in cached_data]
+            random_date = technical_data[random.randint(0, len(technical_data) - 1)]['date']
+
+            assert random_date in dates_array
