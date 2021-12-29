1a1acebd7caba2e3c8440497361b324cceabbdc8
nicky-LV <nickystnkvs@outlook.com>
2021-12-29 23:14:50 +0000

    Refactored DJANGO_SETTINGS_MODULE env variable value.

--- EquityAlpha/celery.py
+++ EquityAlpha/celery.py
@@ -1,10 +1,9 @@
 import os
 from celery import Celery
 
-os.environ["DJANGO_SETTINGS_MODULE"] = "EquityAlpha.settings"
 # Redis message broker
 app = Celery('EquityAlpha', broker="redis://localhost:6379")
 
-# imports Celery-related settings from settings.py, starting with CELERY. (e.g. CELERY_BROKER_URL = ...)
+# imports Celery-related settings from settings-dev.py, starting with CELERY. (e.g. CELERY_BROKER_URL = ...)
 app.config_from_object("Celery.celery_settings", namespace="CELERY")
 app.autodiscover_tasks()
